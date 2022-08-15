fdce4de886795dedc7d7d1b98f581db5145b21f5
SolomonPaulRDMediahuis <110603658+SolomonPaulRDMediahuis@users.noreply.github.com>
2022-08-15 23:00:23 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-euv6yo_-=78718%_!bych$+8_@ah9)6va%9*dgbfcrt1kdu6ko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1:8000]
+ALLOWED_HOSTS = [127.0.0.1:8000, 'assigntoolspaul.herokuapp.com']
 
 
 # Application definition
